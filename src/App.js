3b5300fb4e15a777542f8f339d6755c4a980fbb4
csocrates <calebsocrates@gmail.com>
2021-01-29 16:49:52 +0000

    completes some styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
-import AddHabit from './components/AddHabit';
-import HabitList from './components/HabitList';
+import React from "react";
+import "./App.css";
+import AddHabit from "./components/AddHabit";
+import HabitList from "./components/HabitList";
 
 class App extends React.Component {
-  state = { user: 'Caleb', currentHabits: {} };
+  state = { user: "Caleb", currentHabits: {} };
+
+  componentDidMount() {
+    this.fetchHabits();
+  }
+
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
+      <div className="App">
+        <header className="App-header">
           Welcome Back {this.state.user}
           <h1>The Good Habit Builder</h1>
           <AddHabit addNewHabit={this.addNewHabit} />
@@ -16,6 +21,7 @@ class App extends React.Component {
             currentHabits={this.state.currentHabits}
             handleHabitDone={this.handleHabitDone}
           />
+          <button onClick={this.handleSave}>Save your habits</button>
         </header>
       </div>
     );
@@ -29,8 +35,8 @@ class App extends React.Component {
         return {
           currentHabits: {
             ...currentState.currentHabits,
-            [habit]: { frequency: newFreq - 1 }
-          }
+            [habit]: { frequency: newFreq - 1 },
+          },
         };
     });
   };
@@ -41,14 +47,27 @@ class App extends React.Component {
         return {
           currentHabits: {
             ...currentState.currentHabits,
-            [newHabit.habit]: { frequency: newHabit.frequency }
-          }
+            [newHabit.habit]: { frequency: newHabit.frequency },
+          },
         };
       },
       () => {
         console.log(this.state);
       }
     );
+  };
+  handleSave = () => {
+    localStorage.setItem(
+      "currentHabits",
+      JSON.stringify(this.state.currentHabits)
+    );
+  };
+  fetchHabits = () => {
+    const currentHabits = JSON.parse(localStorage.getItem("currentHabits"));
+    console.log(currentHabits);
+    this.setState(() => {
+      return { currentHabits };
+    });
   };
 }
 
